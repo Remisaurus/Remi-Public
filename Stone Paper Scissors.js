fc3fe1634a500d0717ab394b343214814f485bad
Remisaurus <104589165+Remisaurus@users.noreply.github.com>
2022-07-25 21:16:07 +0200

    Update Stone Paper Scissors.js
    Remimarking

--- Stone Paper Scissors.js
+++ Stone Paper Scissors.js
@@ -1,3 +1,5 @@
+let enough = enough
+
 let A = Math.floor(Math.random()*3);
 let B = Math.floor(Math.random()*3);
 function handA() {let C;
@@ -18,13 +20,13 @@ let D = handB()
 console.log('player1 : '+C)
 console.log('player2 : '+D)
 
-function comparing(){let compare; if (C===D){compare='It is a Tie!'} else if (C==='Scissors'&&D==='Paper'){compare='player1 wins!'} else if (C==='Paper'&&D==='Stone')
-{compare='player1 wins!'} else if 
-(C==='Stone'&&D==='Scissors'){compare='player1 wins!'} else if 
-(D==='Scissors'&&C==='Paper'){compare='player2 wins!'} else if (D==='Paper'&&C==='Stone')
-{compare='player2 wins!'} else if 
-(D==='Stone'&&C==='Scissors'){compare='player2 wins!'};
-return compare}
+function comparing(){let enough; if (C===D){enough='It is a Tie!'} else if (C==='Scissors'&&D==='Paper'){enough='player1 wins!'} else if (C==='Paper'&&D==='Stone')
+{enough='player1 wins!'} else if 
+(C==='Stone'&&D==='Scissors'){enough='player1 wins!'} else if 
+(D==='Scissors'&&C==='Paper'){enough='player2 wins!'} else if (D==='Paper'&&C==='Stone')
+{enough='player2 wins!'} else if 
+(D==='Stone'&&C==='Scissors'){enough='player2 wins!'};
+return enough}
 console.log(comparing())
 
 
