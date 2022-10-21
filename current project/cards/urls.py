adcd39a192825e220f0d303b9d871f8494185c5f
Remisaurus <remi.meijer@gmail.com>
2022-10-21 15:20:31 +0200

    21-10-2022

--- current project/cards/urls.py
+++ current project/cards/urls.py
@@ -7,8 +7,24 @@ from . import views
 
 urlpatterns = [
     path(
+        
         "",
         views.CardListView.as_view(),
-        name="card_list"
+        name="card-list"
+    ),
+    path(
+        "new",
+        views.CardCreateView.as_view(),
+        name="card-create"
+    ),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+    ),
+    path(
+        "box/<int:box_num>",
+        views.BoxView.as_view(),
+        name="box"
     ),
 ]
