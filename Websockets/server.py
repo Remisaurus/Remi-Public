5c29fd26ac50166766b76b400221860403e66cea
Remisaurus <remi.meijer@gmail.com>
2022-11-28 20:37:33 +0100

    28-11-2022

---
+++ Websockets/server.py
@@ -0,0 +1,12 @@
+import asyncio
+import websockets
+import lxml.objectify
+
+async def handle_connection(websocket, path):
+    async for message in websocket:
+        print(message)
+
+if __name__ == "__main__":
+    future = websockets.serve(handle_connection, "localhost", 8000)
+    asyncio.get_event_loop().run_until_complete(future)
+    asyncio.get_event_loop().run_forever()
